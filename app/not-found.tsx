4643dd411f84dac52d04a9ef24370f0a7b1b2aee
Nguyễn Hoàng An <an.nh@odh.vn>
2025-04-01 11:24:35 +0700

    implement: next/intl

---
+++ app/not-found.tsx
@@ -0,0 +1,20 @@
+"use client";
+import { useEffect } from "react";
+import Error from "next/error";
+import Document from "@/components/Document";
+import { useLocale } from "next-intl";
+
+export default function NotFound() {
+  const locale = useLocale();
+
+  // Use client-side effect to handle non-static behavior
+  useEffect(() => {
+    // You can add any client-side logic here if needed
+  }, []);
+
+  return (
+    <Document locale={locale}>
+      <Error statusCode={404} />
+    </Document>
+  );
+}
